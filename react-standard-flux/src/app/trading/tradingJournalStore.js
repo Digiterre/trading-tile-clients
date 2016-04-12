293f093cd4cfd9b616d89dfb24cc60fd713570f2
Uldis Sturms <uldis.sturms@gmail.com>
2016-04-12 10:45:46 +0100

    Reformat on journal store as well

--- react-standard-flux/src/app/trading/tradingJournalStore.js
+++ react-standard-flux/src/app/trading/tradingJournalStore.js
@@ -7,51 +7,49 @@ var CHANGE_EVENT = 'change';
 var utils = require('../core/utils');
 
 function TradingJournalStore() {
-		
-	var self = this;
 
-	var store = {		
-		addChangeListener: addChangeListener,
-		removeChangeListener: removeChangeListener
-         };
+  var self = this;
 
-         init();
+  var store = {
+    addChangeListener: addChangeListener,
+    removeChangeListener: removeChangeListener
+  };
 
-	return store;
+  init();
 
-	////////////////////////
+  return store;
 
-	function init() {
-		
-		Dispatcher.register(function(action) {
-			switch(action.actionType) {
-								
-				case ActionTypes.TRADE_EXECUTED:					
-					onTradeDidExecute(action.trade);
-					break;
-					
-				default:
-					// no op
-			}
-		});
-         }
+  ////////////////////////
 
-         function onTradeDidExecute(trade) {         		
-		self.emit(CHANGE_EVENT, trade);		
-         }
+  function init() {
 
-         
-	function addChangeListener(callback) {		
-		self.on(CHANGE_EVENT, callback);
-	}
+    Dispatcher.register(function(action) {
+      switch (action.actionType) {
+        case ActionTypes.TRADE_EXECUTED:
+          onTradeDidExecute(action.trade);
+          break;
 
-	function removeChangeListener(callback) {
-		self.removeListener(CHANGE_EVENT, callback);	
-          }         
-   
-}       
+        default:
+        // no op
+      }
+    });
+  }
+
+  function onTradeDidExecute(trade) {
+    self.emit(CHANGE_EVENT, trade);
+  }
+
+  function addChangeListener(callback) {
+    self.on(CHANGE_EVENT, callback);
+  }
+
+  function removeChangeListener(callback) {
+    self.removeListener(CHANGE_EVENT, callback);
+  }
+
+}
 
 utils.inherits(TradingJournalStore, EventEmitter);
 
-module.exports = new TradingJournalStore(); 
+module.exports = new TradingJournalStore();
 
