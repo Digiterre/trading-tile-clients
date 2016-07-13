f866719d62c818523c77b7aaee221c096c654bb1
Tom Kearney <tom.kearney@outlook.com>
2016-07-13 15:51:22 +0100

    initialising project

---
+++ ng2-client/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'trading-tile-app',
+    templateUrl: 'app/views/dashboard.component.html'
+})
+export class AppComponent { }
